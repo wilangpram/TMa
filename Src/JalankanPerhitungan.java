4e521ed8633fa6cd37e0adaf18b1c093b6ebd5e6
willyanggip <92911347+willyanggip@users.noreply.github.com>
2022-04-30 15:18:26 +0700

    first commit

---
+++ Src/JalankanPerhitungan.java
@@ -0,0 +1,20 @@
+package Src;
+
+import Model.Perhitungan;
+
+public class JalankanPerhitungan {
+
+    public static void main(String[] args) {
+        Perhitungan s = new Perhitungan(4, 6, 0);
+        String judul = s.PrintJudul("Judul Menggunakan Interface");
+        System.out.println(judul);
+        System.out.println();
+
+        int hasiltambah = s.HitungTambah(4, 6);
+        System.out.println(hasiltambah);
+        System.out.println();
+
+        int hasilkali = s.HitungKali(4, 6);
+        System.out.println(hasilkali);
+    }
+}
